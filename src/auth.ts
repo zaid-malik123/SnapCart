c6b594b1c3df7284574a2d9bdbed66c1ed312d63
mohd zaid malik <mzaidmalik25@gmail.com>
2025-11-28 12:45:26 +0530

    Add NextAuth with Credentials provider, user authentication, JWT/session callbacks, and DB integration

---
+++ src/auth.ts
@@ -0,0 +1,78 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
+import { connectDb } from "./db/db"
+import User from "./models/user.model"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+        credentials: {
+            email: {label : "Email", type: "email"},
+            password: {label: "Password", type: "password"}
+        },
+       async authorize(credentials, request){
+         try {
+             await connectDb()
+             const email = credentials.email
+             const password = credentials.password as string
+
+             const user = await User.findOne({email})
+
+             if(!user){
+                throw new Error("User does not exist")
+             }
+
+             const compare = await bcrypt.compare(password, user.password)
+             if(!compare){
+                throw new Error("Password does not match")
+             }
+
+             return {
+                id: user._id .toString(),
+                email: user.email,
+                name: user.name,
+                role: user.role
+             }
+         } catch (error) {
+             return null
+
+         }
+       }
+    })
+  ],
+
+  callbacks: {
+    jwt({token, user}){
+       if(user){
+         token.id = user.id,
+         token.name = user.name,
+         token.email = user.email,
+         token.role = user.role
+       }
+       return token
+    },
+
+    session({session, token}){
+        if(session.user){
+            session.user.id = token.id as string,
+            session.user.name = token.name as string,
+            session.user.email = token.email as string,
+            session.user.role = token.role as string
+        }
+        return session
+    }
+  } ,
+
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  },
+
+  secret: process.env.AUTH_SECRET
+})
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,11 @@
+
+declare module "next-auth" {
+    interface User {
+        id: string,
+        name: string,
+        email: string,
+        role: string
+    }
+}
+
+export {}
