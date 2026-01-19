05591eaf2747676ffe4767bb24dcec7973cf580b
mohd zaid malik <mzaidmalik25@gmail.com>
2026-01-19 17:28:16 +0530

    feat: create order schema model

---
+++ src/models/oder.model.ts
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+
+interface orderI {
+  id?: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  items: [
+    {
+        grocery: mongoose.Types.ObjectId,
+        name: string,
+        price: string,
+        unit: string,
+        image: string,
+        quantity: number
+    }
+  ];
+  totalAmount: string;
+  paymentMethod: "cod" | "online";
+  address: {
+    fullName: string,
+    mobile: string,
+    city: string,
+    state: string,
+    pincode: string,
+    fullAddress: string,
+    latitude: number,
+    longitude: number
+  },
+  status: "pending" | "out of delivery" | "delivered";
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const orderSchema = new mongoose.Schema<orderI>({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    items: [
+        {
+            grocery: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Grocery",
+                required: true
+            },
+            name: String,
+            price: String,
+            unit: String,
+            image: String,
+            quantity: Number
+        }
+    ],
+    paymentMethod: {
+        type: String,
+        enum: ["cod", "online"],
+        default: "cod"
+    },
+    address: {
+        fullName: String,
+        mobile: String,
+        city: String,
+        state: String,
+        pincode: String,
+        fullAddress: String,
+        latitude: Number,
+        longitude: Number
+    },
+    totalAmount: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "out of delivery", "delivered"],
+        default: "pending"
+    }
+} ,{timestamps: true})
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema)
+export default Order
