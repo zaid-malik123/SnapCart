b3a31d2e6be9fef47d99f6379446e3f9eeb7eb89
mohd zaid malik <mzaidmalik25@gmail.com>
2026-01-20 17:48:09 +0530

    feat: Design order success page with animations and link to my orders

--- src/app/user/order-success/page.tsx
+++ src/app/user/order-success/page.tsx
@@ -1,10 +1,86 @@
+"use client";
 
+import { Package } from "lucide-react";
+import { ArrowRight } from "lucide-react";
+import { CheckCircle } from "lucide-react";
+import { motion } from "motion/react";
+import Link from "next/link";
 const OrderSuccess = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] px-6 text-center bg-gradient-b from-green-50 to-white">
+      <motion.div
+        initial={{ scale: 0, rotate: -180 }}
+        animate={{ scale: 1, rotate: 0 }}
+        transition={{
+          type: "spring",
+          damping: 10,
+          stiffness: 100,
+        }}
+        className="relative"
+      >
+        <CheckCircle className="text-green-600 w-24 h-24 md:w-28 md:h-28" />
+        <motion.div
+          initial={{ opacity: 0, scale: 0.6 }}
+          animate={{
+            opacity: [0, 1, 0, 1, 0],
+            scale: [0.6, 1, 0.6],
+          }}
+          transition={{
+            repeat: Infinity,
+            duration: 2,
+            ease: "easeInOut",
+          }}
+          className="absolute inset-0"
+        >
+          <div className="w-full h-full rounded-full bg-green-200 blur-2xl" />
+        </motion.div>
+      </motion.div>
+      <motion.h1
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.3 }}
+        className="text-3xl md:text-4xl font-bold text-green-700 mt-6"
+      >
+        Order Placed Successfully
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.5 }}
+        className="text-gray-600 mt-3 text-sm md:text-base max-w-md"
+      >
+        Thank You for shopping with us ! Your order has been placed and its
+        being processed. You can track its progress in your{" "}
+        <span className="font-semibold text-green-700">My Orders</span> section
+        .
+      </motion.p>
 
+      <motion.div
+      initial={{y: 20, opacity: 0}}
+      animate={{y: [0, -10, 0], opacity: 1}}
+      transition={{delay: 1, duration: 2, repeat: Infinity, ease: "easeInOut"}}
+      className="mt-10"
+      >
+        <Package className="w-16 h-16 md:w-20 md:h-20 text-green-500"/>
+      </motion.div>
+      <motion.div
+      initial={{opacity: 0, scale: 0.9}}
+      animate={{opacity: 1, scale: 1}}
+      transition={{delay: 1.2, duration: 0.4}}
+      className="mt-12"
+      >
+        <Link href={"/user/my-orders"}>
+          <motion.div
+          whileHover={{scale: 1.03}}
+          whileTap={{scale: 0.93}}
+          className="flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white text-base font-semibold px-8 py-3 rounded-full shoadow-lg transition-all"
+          >
+            Go to My Order Page <ArrowRight/>
+          </motion.div>
+        </Link>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderSuccess
+export default OrderSuccess;
